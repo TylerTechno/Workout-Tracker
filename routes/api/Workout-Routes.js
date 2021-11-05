fb0963cb4e10bc608018f33a7e6f6b36d20c17e2
Tyler Everett <tylertechnotrade@gmail.com>
2021-11-05 16:45:33 -0700

    new commit

--- routes/api/Workout-Routes.js
+++ routes/api/Workout-Routes.js
@@ -1,12 +1,44 @@
+const { Workout } = require("../../models")
+
 const router = require("express").Router()
 //this route is api/workouts
-router.get("/" , async (req, res) => {
+router.get("/", async (req, res) => {
     try {
-        
+        var workoutData = await Workout.find()
+        res.json(workoutData)
     } catch (error) {
         res.status(500).json(error)
     }
-} )
+})
+
+
+router.post("/", async (req, res) => {
+    try {
+        var workoutData = await Workout.create(req.body)
+        res.json(workoutData)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+router.put("/:id", async (req, res) => {
+    try {
+        var workoutData = await Workout.findByIdAndUpdate(req.params.id, {
+            $push: {
+                exercises: req.body
+            }
+
+        })
+        res.json(workoutData)
+
+
+    } catch (error) {
+        res.status(500).json(error)
+
+    }
+})
+
+
 
 
 module.exports = router
